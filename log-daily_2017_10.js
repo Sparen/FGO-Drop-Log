1fdaf5d86cae79367ae204dc7e92191df3ec93c3
Andrew Fan <andrewsuicune@gmail.com>
2017-11-27 23:21:46 -0500

    Logged A40

--- log-daily_2017_10.js
+++ log-daily_2017_10.js
@@ -102,7 +102,7 @@ var daily_archer_2017_10_obj = {
             ]
         },
         {
-            "qname": "Archer Training Ground - Expert", "ap": "40", "last-upd": "2017 11 06", "column": "30",
+            "qname": "Archer Training Ground - Expert", "ap": "40", "last-upd": "2017 11 27", "column": "30",
             "droplog": [
                 { "uplog": true, "drop": ["GEM_BL_ARC", "GEM_RD_ARC", "GEM_GD_ARC", "MONU_ARC", "MONU_ARC"] },
                 { "uplog": true, "drop": ["GEM_RD_ARC", "PIECE_ARC", "PROOF_OF_HERO"] },
@@ -114,7 +114,12 @@ var daily_archer_2017_10_obj = {
                 { "uplog": true, "drop": ["GEM_RD_ARC", "GEM_RD_ARC", "PIECE_ARC"] },
                 { "uplog": true, "drop": ["GEM_BL_ARC", "GEM_BL_ARC", "GEM_RD_ARC", "PIECE_ARC", "PIECE_ARC", "VOIDS_DUST"] },
                 { "uplog": true, "drop": ["GEM_RD_ARC", "MONU_ARC", "EXP_3_ARC", "PROOF_OF_HERO", "SEED_OF_YGGDRASIL"] },
-                { "uplog": true, "drop": ["GEM_RD_ARC", "GEM_GD_ARC", "MONU_ARC"] }
+                { "uplog": true, "drop": ["GEM_RD_ARC", "GEM_GD_ARC", "MONU_ARC"] },
+                { "uplog": true, "drop": ["GEM_RD_ARC", "GEM_GD_ARC", "PIECE_ARC", "PHOENIX_FEATHER"] },
+                { "uplog": true, "drop": ["GEM_RD_ARC", "PIECE_ARC", "EXP_3_ARC", "VOIDS_DUST", "PHOENIX_FEATHER"] },
+                { "uplog": true, "drop": ["GEM_RD_ARC", "PIECE_ARC", "MONU_ARC", "PHOENIX_FEATHER"] },
+                { "uplog": true, "drop": ["GEM_BL_ARC", "GEM_RD_ARC", "GEM_RD_ARC", "PIECE_ARC"] },
+                { "uplog": true, "drop": ["GEM_RD_ARC", "PIECE_ARC", "MONU_ARC", "EXP_3_ARC", "PROOF_OF_HERO"] }
             ]
         }
     ]
