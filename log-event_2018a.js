f74abedf0fb91ed9f46e558a960af2730ca72993
Andrew Fan <andrewsuicune@gmail.com>
2018-01-05 18:20:11 -0500

    MOAR SABER WARS

--- log-event_2018a.js
+++ log-event_2018a.js
@@ -5,17 +5,25 @@ var event_saberwars2018_obj = {
     "cssclass": "h-saberwars",
     "quests": [
         {
-            "qname": "Equipment Gathering - Novice", "ap": "20", "last-upd": "2018 01 04", "column": "32",
+            "qname": "Equipment Gathering - Novice", "ap": "20", "last-upd": "2018 01 05", "column": "32",
             "droplog": [
                 { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 4}, {"id": "AP_LENS", "stack": 2}] },
                 { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 4}, {"id": "AP_LENS", "stack": 2}] },
                 { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 4}, {"id": "AP_LENS", "stack": 2}] },
-                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 4}, {"id": "AP_LENS", "stack": 2}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 4}, {"id": "AP_LENS", "stack": 2}] },
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "ALTRIUM", "stack": 100}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 2}, {"id": "TRANSISTOR", "stack": 4}, {"id": "AP_LENS", "stack": 2}] }
             ]
         },
         {
-            "qname": "Equipment Gathering - Intermediate", "ap": "25", "last-upd": "2018 01 04", "column": "32",
+            "qname": "Equipment Gathering - Intermediate", "ap": "25", "last-upd": "2018 01 05", "column": "32",
             "droplog": [
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}] },
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}] },
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}] },
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}] },
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}] },
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}] },
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}] },
                 { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 2}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}] }
             ]
         },
@@ -28,12 +36,23 @@ var event_saberwars2018_obj = {
         {
             "qname": "Equipment Gathering - Expert", "ap": "40", "last-upd": "2018 01 04", "column": "32",
             "droplog": [
-                { "uplog": true, "drop": ["OCTUPLET_CRYSTAL"], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 400}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 4}] },
-
+                { "uplog": true, "drop": ["OCTUPLET_CRYSTAL"], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 400}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 2}, {"id": "VACUUM_TUBE", "stack": 4}] }
             ]
         },
         {
-            "qname": "Equipment Gathering - Comet", "ap": "40", "last-upd": "", "column": "32",
+            "qname": "Equipment Gathering - Comet", "ap": "40", "last-upd": "2018 01 05", "column": "32",
+            "droplog": [
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 200}, {"id": "ALTRIUM", "stack": 300}, {"id": "ALTRIUM", "stack": 300}, {"id": "ALTRIUM", "stack": 300}, {"id": "ALTRIUM", "stack": 400}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 6}] }
+            ]
+        },
+        {
+            "qname": "Equipment Gathering - Planet", "ap": "40", "last-upd": "2018 01 05", "column": "32",
+            "droplog": [
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "ALTRIUM", "stack": 600}, {"id": "ALTRIUM", "stack": 600}, {"id": "ALTRIUM", "stack": 600}, {"id": "ALTRIUM", "stack": 600}, {"id": "TRANSISTOR", "stack": 4}, {"id": "TRANSISTOR", "stack": 4}, {"id": "TRANSISTOR", "stack": 4}, {"id": "TRANSISTOR", "stack": 4}, {"id": "TRANSISTOR", "stack": 4}, {"id": "TRANSISTOR", "stack": 4}, {"id": "AP_LENS", "stack": 4}, {"id": "VACUUM_TUBE", "stack": 8}] }
+            ]
+        },
+        {
+            "qname": "Equipment Gathering - Star Cluster", "ap": "40", "last-upd": "", "column": "32",
             "droplog": [
             ]
         }
