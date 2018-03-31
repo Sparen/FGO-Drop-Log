c2ccb6dc6788ce72ce8e5f095ccd5e048e3860ac
Andrew Fan <andrewsuicune@gmail.com>
2018-03-31 15:51:01 -0400

    Reorganization

---
+++ servant-list-part0.js
@@ -0,0 +1,42 @@
+var servantlist_obj_release = {
+    "servants": [
+        {
+            "idnum": "001",
+            "sicon": "shielder-mash.png",
+            "ename": "Mash Kyrielight",
+            "jname1": "マシュ・キリエライト",
+            "cicon": "shielder-s.png",
+            "class": "Shielder",
+            "rarity": "3-4",
+            "deck": ["Q", "A", "A", "B", "B"],
+            "deckhits": [2, 2, 1, 3],
+            "np": "A",
+            "starabs": "99",
+            "stargen": "9.9%",
+            "npcharge": ["0.84%", "3.0%"],
+            "gender": "Female",
+            "ess": "Earth",
+            "alignment": "Lawful Good",
+            "traits": ["Humanoid - Riding", "Weak to Enuma Elish"]
+        },
+        {
+            "idnum": "002",
+            "sicon": "saber-altria.png",
+            "ename": "Altria Pendragon",
+            "jname1": "アルトリア・ペンドラゴン",
+            "cicon": "saber-g.png",
+            "class": "Saber",
+            "rarity": "5",
+            "deck": ["Q", "A", "A", "B", "B"],
+            "deckhits": [2, 2, 1, 3],
+            "np": "B",
+            "starabs": "102",
+            "stargen": "10%",
+            "npcharge": ["0.86%", "3.0%"],
+            "gender": "Female",
+            "ess": "Earth",
+            "alignment": "Lawful Good",
+            "traits": ["Altria - Dragon - Humanoid - King - Riding - Saberface", "Weak to Enuma Elish"]
+        }
+    ]
+};
