22e82706b24e21b5ffe3501786a16f20d89d15be
Andrew Fan <andrewsuicune@gmail.com>
2018-04-16 00:01:45 -0400

    Amazoness 40AP Hunting Q logs

--- log-event_hunting.js
+++ log-event_hunting.js
@@ -5,19 +5,19 @@ var event_hunting1_obj = {
     "cssclass": "h-hunting",
     "quests": [
         {
-            "qname": "[2018 04 14] Skeleton Hunt - Cause Rank", "ap": "30", "last-upd": "", "column": "",
+            "qname": "[2018 04 14] Skeleton Hunt - Cause Rank [ARC]", "ap": "30", "last-upd": "", "column": "",
             "droplog": [
                 { "uplog": false, "drop": ["GEM_BL_ARC", "GEM_RD_ARC", "GEM_RD_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC"] }
             ]
         },
         {
-            "qname": "[2018 04 14] Skeleton Hunt - Fes Rank", "ap": "35", "last-upd": "", "column": "",
+            "qname": "[2018 04 14] Skeleton Hunt - Fes Rank [ARC]", "ap": "35", "last-upd": "", "column": "",
             "droplog": [
                 { "uplog": false, "drop": ["EVIL_BONE", "EVIL_BONE", "EVIL_BONE", "EVIL_BONE", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC"] }
             ]
         },
         {
-            "qname": "[2018 04 14] Skeleton Hunt - Pride Rank", "ap": "40", "last-upd": "2018 04 14", "column": "32",
+            "qname": "[2018 04 14] Skeleton Hunt - Pride Rank [ARC]", "ap": "40", "last-upd": "2018 04 14", "column": "32",
             "droplog": [
                 { "uplog": true, "drop": ["GEM_BL_ARC", "GEM_RD_ARC", "GEM_RD_ARC", "EVIL_BONE", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC"] },
                 { "uplog": true, "drop": ["GEM_RD_ARC", "EVIL_BONE", "EVIL_BONE", "EVIL_BONE", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC"] },
@@ -29,6 +29,51 @@ var event_hunting1_obj = {
                 { "uplog": true, "drop": ["GEM_RD_ARC", "EVIL_BONE", "EVIL_BONE", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC"] },
                 { "uplog": true, "drop": ["GEM_BL_ARC", "GEM_RD_ARC", "EVIL_BONE", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC" ] },
                 { "uplog": true, "drop": ["GEM_RD_ARC", "EVIL_BONE", "EVIL_BONE", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC", "EXP_4_ARC" ] }
+            ]
+        },
+        {
+            "qname": "[2018 04 15] Amazoness Hunt - Cause Rank [SAB]", "ap": "30", "last-upd": "", "column": "",
+            "droplog": [
+                { "uplog": false, "drop": ["GEM_BL_SAB", "GEM_GD_SAB", "PHOENIX_FEATHER", "EXP_4_SAB", "EXP_4_SAB"] }
+            ]
+        },
+        {
+            "qname": "[2018 04 15] Amazoness Hunt - Fes Rank [SAB]", "ap": "35", "last-upd": "", "column": "",
+            "droplog": [
+                { "uplog": false, "drop": ["PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB"] }
+            ]
+        },
+        {
+            "qname": "[2018 04 15] Amazoness Hunt - Pride Rank [SAB]", "ap": "40", "last-upd": "2018 04 15", "column": "32",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_SAB", "GEM_GD_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_SAB", "GEM_RD_SAB", "PHOENIX_FEATHER", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_RD_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_RD_SAB", "GEM_GD_SAB", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_GD_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_RD_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_RD_SAB", "PHOENIX_FEATHER", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_GD_SAB", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "PHOENIX_FEATHER", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "PHOENIX_FEATHER", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_RD_SAB", "GEM_RD_SAB", "SEED_OF_YGGDRASIL", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_GD_SAB", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_RD_SAB", "GEM_GD_SAB", "PHOENIX_FEATHER", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["PHOENIX_FEATHER", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_RD_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_RD_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["PHOENIX_FEATHER", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_RD_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_GD_SAB", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_RD_SAB", "GEM_GD_SAB", "PHOENIX_FEATHER", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["PHOENIX_FEATHER", "PHOENIX_FEATHER", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["GEM_RD_SAB", "GEM_GD_SAB", "SEED_OF_YGGDRASIL", "EXP_4_SAB", "EXP_4_SAB", "EXP_4_SAB"] }
             ]
         }
     ]
