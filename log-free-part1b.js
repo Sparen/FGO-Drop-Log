7e5b2bedcc75a9fb80aa47a1fd67a7e995e890cc
Andrew Fan <andrewsuicune@gmail.com>
2018-07-19 11:25:31 -0400

    Split free quests into two files with four singularities each

---
+++ log-free-part1b.js
@@ -0,0 +1,367 @@
+/* ----- ----- ----- ----- ----- FREE QUESTS ----- ----- ----- ----- ----- */
+// _LONDON
+var free_london_obj = {
+    "cssclass": "h-london",
+    "quests": [
+        {
+            "qname": "Old Street: Fog, Carriages, and Gaslights", "ap": "15", "last-upd": "2017 12 20", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["ETERNAL_GEAR"] }
+            ]
+        },
+        {
+            "qname": "White Chapel: Slum", "ap": "15", "last-upd": "2017 12 21", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_ASS"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_ASS", "GEM_BL_ASS"] }
+            ]
+        },
+        {
+            "qname": "City of London: Square Mile", "ap": "15", "last-upd": "2017 12 22", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_LAN"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Soho: Charming Downtown Area", "ap": "16", "last-upd": "2017 12 21", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_LAN", "MONU_CAS"] }
+            ]
+        },
+        {
+            "qname": "Westminster: Scotland Yard", "ap": "16", "last-upd": "2017 12 22", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["HOMUNCULUS_BABY"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Regent Park: Royal Family's Hunting Ground", "ap": "16", "last-upd": "2017 12 21", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_CAS", "ETERNAL_GEAR"] },
+                { "uplog": true, "drop": ["GEM_BL_CAS", "GEM_RD_SAB"] }
+            ]
+        },
+        {
+            "qname": "Clerkenwell: Soothing Well", "ap": "18", "last-upd": "2018 01 04", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_ASS"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_RD_ASS"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "ETERNAL_GEAR"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_SAB", "GEM_RD_ASS"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB"] },
+                { "uplog": true, "drop": ["EVIL_BONE"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_SAB", "GEM_BL_SAB", "GEM_GD_ASS"] },
+                { "uplog": true, "drop": ["EVIL_BONE", "EVIL_BONE"] },
+                { "uplog": true, "drop": ["EVIL_BONE"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["ETERNAL_GEAR"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB"] },
+                { "uplog": true, "drop": ["EVIL_BONE"] },
+                { "uplog": true, "drop": ["EVIL_BONE", "ETERNAL_GEAR"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["EVIL_BONE", "EVIL_BONE", "ETERNAL_GEAR"] },
+                { "uplog": true, "drop": ["GEM_RD_ASS"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "ETERNAL_GEAR"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["EVIL_BONE"] },
+                { "uplog": true, "drop": ["EVIL_BONE"] },
+                { "uplog": true, "drop": ["EVIL_BONE", "ETERNAL_GEAR"] }
+            ]
+        },
+        {
+            "qname": "Southwark: Borough Market", "ap": "19", "last-upd": "2017 12 22", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_LAN", "HOMUNCULUS_BABY"] },
+                { "uplog": true, "drop": ["GEM_RD_LAN", "HOMUNCULUS_BABY"] },
+                { "uplog": true, "drop": ["HOMUNCULUS_BABY"] }
+            ]
+        },
+        {
+            "qname": "Hyde Park: Vestiges of the Crystal Palace", "ap": "20", "last-upd": "2018 01 21", "column": "21",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_LAN", "GEM_RD_CAS"] },
+                { "uplog": true, "drop": ["FORBIDDEN_PAGE"] }
+            ]
+        }
+    ]
+};
+
+// _EPU
+var free_epu_obj = {
+    "cssclass": "h-epu",
+    "quests": [
+        {
+            "qname": "Black Hills: Holy Mountain", "ap": "17", "last-upd": "2018 03 20", "column": "3",
+            "droplog": [
+                { "uplog": true, "drop": ["ETERNAL_GEAR"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Riverton: The Great Riverbed", "ap": "17", "last-upd": "2018 03 20", "column": "3",
+            "droplog": [
+                { "uplog": true, "drop": ["VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PIECE_LAN"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "VOIDS_DUST"] }
+            ]
+        },
+        {
+            "qname": "Denver: Mile High City", "ap": "17", "last-upd": "2018 03 21", "column": "3", "icon": "lancer-karna.png",
+            "droplog": [
+                { "uplog": true, "drop": ["PIECE_LAN"] },
+                { "uplog": true, "drop": ["PIECE_LAN"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Deming: New Chicago", "ap": "17", "last-upd": "2018 03 22", "column": "6",
+            "droplog": [
+                { "uplog": true, "drop": ["DRAGON_FANG"] },
+                { "uplog": true, "drop": ["GEM_RD_RID"] },
+                { "uplog": true, "drop": ["DRAGON_FANG"] }
+            ]
+        },
+        {
+            "qname": "Lubbock: Texas Ranger", "ap": "18", "last-upd": "2018 03 24", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_RD_ASS"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_SAB", "GEM_RD_ASS", "PROOF_OF_HERO"] }
+            ]
+        },
+        {
+            "qname": "Dallas: Cowtown Colosseum", "ap": "17", "last-upd": "2018 03 25", "column": "3",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO"] }
+            ]
+        },
+        {
+            "qname": "Alexandria: Weeks Island", "ap": "18", "last-upd": "2018 04 01", "column": "6",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "GEM_BL_LAN", "GEM_BL_ARC", "PROOF_OF_HERO"] }
+            ]
+        },
+        {
+            "qname": "Alcatraz: Prison Island", "ap": "18", "last-upd": "2018 04 05", "column": "3", "icon": "berserker-beowulf.png",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["VOIDS_DUST"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Des Moines: Surveillance Fortress", "ap": "18", "last-upd": "2018 04 05", "column": "3", "icon": "lancer-lishuwen.png",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["SEED_OF_YGGDRASIL"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Kearney: Great Plains", "ap": "18", "last-upd": "2018 04 05", "column": "18",
+            "droplog": [
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "WARHORSE_YOUNG_HORN"] },
+                { "uplog": true, "drop": ["GEM_BL_LAN", "GEM_RD_LAN", "PROOF_OF_HERO"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Montgomery: Heart of Dixie", "ap": "18", "last-upd": "2018 05 17", "column": "8", "icon": "archer-arjuna.png",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_ARC"] },
+                { "uplog": true, "drop": ["GEM_BL_ARC", "PIECE_ARC"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "VOIDS_DUST"] }
+            ]
+        },
+        {
+            "qname": "Charlotte: Gold Rush", "ap": "20", "last-upd": "2018 05 29", "column": "6",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_CAS", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "VOIDS_DUST", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_BL_CAS", "PIECE_BER", "MONU_BER", "PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO"] },
+                { "uplog": true, "drop": ["VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_BL_CAS"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_BL_ARC"] },
+                { "uplog": true, "drop": ["GEM_BL_CAS", "MONU_BER", "PROOF_OF_HERO"] },
+                { "uplog": true, "drop": ["PIECE_BER", "PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_BL_LAN", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["MONU_LAN"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "PROOF_OF_HERO", "PROOF_OF_HERO", "PROOF_OF_HERO", "VOIDS_DUST", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PIECE_BER", "MONU_LAN", "PROOF_OF_HERO"] },
+                { "uplog": true, "drop": ["MONU_BER", "PROOF_OF_HERO", "PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO"] },
+                { "uplog": true, "drop": ["GEM_BL_LAN", "PIECE_LAN"] },
+                { "uplog": true, "drop": ["PIECE_BER", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PIECE_BER", "PROOF_OF_HERO"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_LAN", "GEM_BL_ARC", "PIECE_LAN", "PIECE_BER", "PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["MONU_BER"] },
+                { "uplog": true, "drop": ["VOIDS_DUST"] },
+                { "uplog": true, "drop": ["MONU_LAN"] },
+                { "uplog": true, "drop": ["GEM_BL_ARC", "PIECE_BER"] },
+                { "uplog": true, "drop": ["VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_BL_ARC", "PROOF_OF_HERO", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_BL_LAN", "PIECE_BER"] },
+                { "uplog": true, "drop": ["VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PIECE_BER"] }
+            ]
+        },
+        {
+            "qname": "Washington: Special Administrative Area", "ap": "20", "last-upd": "2018 05 17", "column": "6",
+            "droplog": [
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "EXP_4_SAB"] },
+                { "uplog": true, "drop": ["EXP_4_SAB"] },
+                { "uplog": true, "drop": ["PROOF_OF_HERO", "PROOF_OF_HERO", "EXP_4_SAB"] }
+            ]
+        },
+        {
+            "qname": "Chicago: Windy City", "ap": "21", "last-upd": "2018 05 17", "column": "11",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["ETERNAL_GEAR"] },
+                { "uplog": true, "drop": ["GEM_BL_ARC"] }
+            ]
+        }
+    ]
+};
+
+// _CAMELOT
+var free_camelot_obj = {
+    "cssclass": "h-camelot",
+    "quests": [
+        {
+            "qname": "Sandstorm Desert: Garden of the Sphinx", "ap": "19", "last-upd": "2018 06 30", "column": "2",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_ARC", "GEM_GD_CAS", "EVIL_BONE", "EVIL_BONE"] },
+                { "uplog": true, "drop": ["GEM_BL_SAB", "EXP_4_CAS"] },
+                { "uplog": true, "drop": ["GEM_GD_CAS"] }
+            ]
+        },
+        {
+            "qname": "Dune of Dawn: The Incandescent Crest", "ap": "19", "last-upd": "2018 06 30", "column": "2",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_CAS", "GEM_BL_CAS", "EXP_4_CAS", "SERPENT_JEWEL"] },
+                { "uplog": true, "drop": ["EXP_4_CAS", "SERPENT_JEWEL"] },
+                { "uplog": true, "drop": ["EXP_4_CAS"] }
+            ]
+        },
+        {
+            "qname": "Wastelands of Death: The Mad Banquet of Blood and Flesh", "ap": "19", "last-upd": "2018 07 05", "column": "5",
+            "droplog": [
+                { "uplog": true, "drop": ["FOOLS_CHAIN"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "East Village: The Withered Mountain Ridge", "ap": "19", "last-upd": "2018 07 18", "column": "3",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "DRAGON_FANG"] }
+            ]
+        },
+        {
+            "qname": "Fortress of the Round Table: The Raiding Party of Knights", "ap": "19", "last-upd": "2018 07 18", "column": "3",
+            "droplog": [
+                { "uplog": true, "drop": ["PROOF_OF_HERO"] },
+                { "uplog": true, "drop": ["GEM_BL_LAN"] },
+                { "uplog": true, "drop": ["GEM_RD_SAB", "PROOF_OF_HERO", "PROOF_OF_HERO"] }
+            ]
+        },
+        {
+            "qname": "Mausoleum of the Evening Bell: The Angel of Death", "ap": "19", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "West Village Ruins: Traces of Cruelty", "ap": "20", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Atlas Institute: Cellar of Giants", "ap": "20", "last-upd": "2018 07 18", "column": "4",
+            "droplog": [
+                 { "uplog": true, "drop": ["EXP_4_ARC"] },
+                 { "uplog": true, "drop": ["FORBIDDEN_PAGE"] },
+                 { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Main Gate: The Gate of Consecration", "ap": "20", "last-upd": "2018 07 18", "column": "7",
+            "droplog": [
+                { "uplog": true, "drop": ["EXP_4_LAN"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GREAT_KNIGHT_MEDAL"] }
+            ]
+        },
+        {
+            "qname": "Holy City: The Ideal City", "ap": "20", "last-upd": "2018 07 18", "column": "4",
+            "droplog": [
+                { "uplog": true, "drop": ["EXP_4_SAB", "PROOF_OF_HERO"] },
+                { "uplog": true, "drop": ["EXP_4_SAB"] },
+                { "uplog": true, "drop": ["OCTUPLET_CRYSTAL", "SPIRIT_ROOT"] }
+            ]
+        },
+        {
+            "qname": "Hidden Village: Mirage of Dreams", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Royal Castle: The Thousand Year Kingdom", "ap": "21", "last-upd": "2018 07 18", "column": "16", "icon": "saber-gawain.png",
+            "droplog": [
+                { "uplog": true, "drop": ["GREAT_KNIGHT_MEDAL"] },
+                { "uplog": true, "drop": ["EXP_4_SAB", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["PIECE_SAB", "VOIDS_DUST"] }
+            ]
+        },
+        {
+            "qname": "Land of the Void: Desolate World", "ap": "22", "last-upd": "2018 07 18", "column": "3",
+            "droplog": [
+                { "uplog": true, "drop": ["METEORIC_HORSESHOE", "METEORIC_HORSESHOE"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "METEORIC_HORSESHOE", "METEORIC_HORSESHOE"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Great Temple: Castle of the Sun King", "ap": "22", "last-upd": "2018 07 18", "column": "4",
+            "droplog": [
+                { "uplog": true, "drop": ["EXP_4_CAS", "EXP_4_CAS", "FOOLS_CHAIN"] },
+                { "uplog": true, "drop": ["EXP_4_CAS", "EXP_4_CAS"] },
+                { "uplog": true, "drop": ["EXP_4_CAS", "EXP_4_CAS"] }
+            ]
+        }
+    ]
+};
