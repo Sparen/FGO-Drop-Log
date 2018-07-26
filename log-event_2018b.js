db4da04bfeedb26fc4e67d8d780376a9675185b4
Andrew Fan <andrewsuicune@gmail.com>
2018-07-26 08:31:49 -0400

    Began logging setup

--- log-event_2018b.js
+++ log-event_2018b.js
@@ -1,9 +1,70 @@
 /* ----- ----- ----- ----- ----- EVENT QUESTS ----- ----- ----- ----- ----- */
 
 // _SUMMER2018
-var event_summer2018_obj = {
+var event_summer2018part1_obj = {
     "cssclass": "h-summer",
     "quests": [
-        
+        {
+            "qname": "Private Beach - Novice", "ap": "10", "last-upd": "", "column": "",
+            "droplog": [
+                { "uplog": false, "drop": [], "stackdrop": [{"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FOOD", "stack": 1}] }
+            ]
+        },
+        {
+            "qname": "Private Beach - Intermediate", "ap": "20", "last-upd": "", "column": "",
+            "droplog": [
+                { "uplog": false, "drop": [], "stackdrop": [{"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}] }
+            ]
+        },
+        {
+            "qname": "Private Beach - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Picnic Field - Novice", "ap": "10", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Picnic Field - Intermediate", "ap": "20", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Picnic Field - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Romantic Cave - Novice", "ap": "10", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Romantic Cave - Intermediate", "ap": "20", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Romantic Cave - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Jungle Adventure - Novice", "ap": "10", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Jungle Adventure - Intermediate", "ap": "20", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Jungle Adventure - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        }
     ]
 };
