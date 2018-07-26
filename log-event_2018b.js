f517bfe9922721bd9ea2b3ec5166fa71edf4eb36
Andrew Fan <andrewsuicune@gmail.com>
2018-07-25 22:32:27 -0400

    Began basic preparations for Summer Event

---
+++ log-event_2018b.js
@@ -0,0 +1,9 @@
+/* ----- ----- ----- ----- ----- EVENT QUESTS ----- ----- ----- ----- ----- */
+
+// _SUMMER2018
+var event_summer2018_obj = {
+    "cssclass": "h-summer",
+    "quests": [
+        
+    ]
+};
