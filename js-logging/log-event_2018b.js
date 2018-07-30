319e65584694d555bdf4e6f43f640f6f0e3c15c1
Andrew Fan <andrewsuicune@gmail.com>
2018-07-29 20:46:11 -0400

    Uplogged all Expert and Advanced nodes for Summer 2018

--- js-logging/log-event_2018b.js
+++ js-logging/log-event_2018b.js
@@ -17,15 +17,15 @@ var event_summer2018part1_obj = {
             ]
         },
         {
-            "qname": "Private Beach - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "qname": "Private Beach - Advanced", "ap": "30", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": [], "stackdrop": [{"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 2}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 2}] }
             ]
         },
         {
-            "qname": "Private Beach - Expert", "ap": "40", "last-upd": "", "column": "",
+            "qname": "Private Beach - Expert", "ap": "40", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": ["GEM_RD_SAB"], "stackdrop": [{"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 4}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}] }
+                { "uplog": true, "drop": ["GEM_RD_SAB"], "stackdrop": [{"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 4}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}] }
             ]
         },
         {
@@ -49,15 +49,15 @@ var event_summer2018part1_obj = {
             ]
         },
         {
-            "qname": "Picnic Field - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "qname": "Picnic Field - Advanced", "ap": "30", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": [], "stackdrop": [{"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}] }
             ]
         },
         {
-            "qname": "Picnic Field - Expert", "ap": "40", "last-upd": "", "column": "",
+            "qname": "Picnic Field - Expert", "ap": "40", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": [], "stackdrop": [{"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 4}, {"id": "STONE", "stack": 4}, {"id": "STONE", "stack": 4}, {"id": "STONE", "stack": 4}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 4}, {"id": "STONE", "stack": 4}, {"id": "STONE", "stack": 4}, {"id": "STONE", "stack": 4}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}] }
             ]
         },
         {
@@ -79,21 +79,21 @@ var event_summer2018part1_obj = {
             ]
         },
         {
-            "qname": "Romantic Cave - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "qname": "Romantic Cave - Advanced", "ap": "30", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": [], "stackdrop": [{"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}] }
             ]
         },
         {
-            "qname": "Romantic Cave - Expert", "ap": "40", "last-upd": "", "column": "",
+            "qname": "Romantic Cave - Expert", "ap": "40", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": ["GEM_RD_BER"], "stackdrop": [{"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}] }
+                { "uplog": true, "drop": ["GEM_RD_BER"], "stackdrop": [{"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 1}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}] }
             ]
         },
         {
             "qname": "Romantic Cave - Thunder", "ap": "40", "last-upd": "", "column": "",
             "droplog": [
-                   { "uplog": false, "drop": ["GEM_RD_BER", "SHELL_OF_REMINISCENCE"], "stackdrop": [{"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}] }
+                { "uplog": false, "drop": ["GEM_RD_BER", "SHELL_OF_REMINISCENCE"], "stackdrop": [{"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "IRON", "stack": 4}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}] }
             ]
         },
         {
@@ -109,15 +109,15 @@ var event_summer2018part1_obj = {
             ]
         },
         {
-            "qname": "Jungle Adventure - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "qname": "Jungle Adventure - Advanced", "ap": "30", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": [], "stackdrop": [{"id": "LUMBER", "stack": 2}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "LUMBER", "stack": 2}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}] }
             ]
         },
         {
-            "qname": "Jungle Adventure - Expert", "ap": "40", "last-upd": "", "column": "",
+            "qname": "Jungle Adventure - Expert", "ap": "40", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": ["SERPENT_JEWEL"], "stackdrop": [{"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 2}] }
+                { "uplog": true, "drop": ["SERPENT_JEWEL"], "stackdrop": [{"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 1}, {"id": "FOOD", "stack": 2}] }
             ]
         },
         {
@@ -141,15 +141,15 @@ var event_summer2018part1_obj = {
             ]
         },
         {
-            "qname": "View Point - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "qname": "View Point - Advanced", "ap": "30", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": ["DRAGON_FANG"], "stackdrop": [{"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}] }
+                { "uplog": true, "drop": ["DRAGON_FANG"], "stackdrop": [{"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "LUMBER", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "STONE", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}, {"id": "IRON", "stack": 2}] }
             ]
         },
         {
-            "qname": "View Point - Expert", "ap": "40", "last-upd": "", "column": "",
+            "qname": "View Point - Expert", "ap": "40", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": [], "stackdrop": [{"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "STONE", "stack": 3}, {"id": "STONE", "stack": 3}, {"id": "STONE", "stack": 4}, {"id": "IRON", "stack": 3}, {"id": "IRON", "stack": 3}, {"id": "IRON", "stack": 3}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 3}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "LUMBER", "stack": 4}, {"id": "STONE", "stack": 3}, {"id": "STONE", "stack": 3}, {"id": "STONE", "stack": 4}, {"id": "IRON", "stack": 3}, {"id": "IRON", "stack": 3}, {"id": "IRON", "stack": 3}] }
             ]
         },
         {
@@ -171,15 +171,15 @@ var event_summer2018part1_obj = {
             ]
         },
         {
-            "qname": "Mystery Zone - Advanced", "ap": "30", "last-upd": "", "column": "",
+            "qname": "Mystery Zone - Advanced", "ap": "30", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": [], "stackdrop": [{"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}] }
+                { "uplog": true, "drop": [], "stackdrop": [{"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FRESH_WATER", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}, {"id": "FOOD", "stack": 2}] }
             ]
         },
         {
-            "qname": "Mystery Zone - Expert", "ap": "40", "last-upd": "", "column": "",
+            "qname": "Mystery Zone - Expert", "ap": "40", "last-upd": "2018 07 29", "column": "8",
             "droplog": [
-                { "uplog": false, "drop": ["GEM_RD_BER", "GEM_GD_BER"], "stackdrop": [{"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 4}, {"id": "FOOD", "stack": 4}, {"id": "FOOD", "stack": 4}, {"id": "FOOD", "stack": 4}] }
+                { "uplog": true, "drop": ["GEM_RD_BER", "GEM_GD_BER"], "stackdrop": [{"id": "FRESH_WATER", "stack": 3}, {"id": "FRESH_WATER", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 3}, {"id": "FOOD", "stack": 4}, {"id": "FOOD", "stack": 4}, {"id": "FOOD", "stack": 4}, {"id": "FOOD", "stack": 4}] }
             ]
         },
         {
