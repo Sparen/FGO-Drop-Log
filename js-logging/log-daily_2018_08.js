4f10c92dc076a889376c52c7dcb086699bb14296
Andrew Fan <andrewsuicune@gmail.com>
2018-08-16 22:24:41 -0400

    New Rider dailies uplogged

--- js-logging/log-daily_2018_08.js
+++ js-logging/log-daily_2018_08.js
@@ -82,23 +82,72 @@ var daily_rider_2018_08_obj = {
     "cssclass": "h-daily-cavalry1",
     "quests": [
         {
-            "qname": "[201808] Rider Training Ground - Novice", "ap": "10", "last-upd": "", "column": "", "icon": "rider-medusa.png",
+            "qname": "[201808] Rider Training Ground - Novice", "ap": "10", "last-upd": "2018 08 16", "column": "18", "icon": "rider-medusa.png",
             "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_RID", "METEORIC_HORSESHOE", "EXP_1_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "PIECE_RID"] },
+                { "uplog": true, "drop": ["DRAGON_FANG", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "PIECE_RID", "DRAGON_FANG"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "GEM_BL_RID", "GEM_BL_RID", "PIECE_RID", "EXP_2_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_3_RID", "DRAGON_FANG"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_2_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["PIECE_RID"] },
+                { "uplog": true, "drop": ["DRAGON_FANG", "EXP_2_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_2_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "PIECE_RID", "DRAGON_FANG", "EXP_1_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "PIECE_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_1_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["DRAGON_FANG", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "EXP_2_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "PIECE_RID"] },
+                { "uplog": true, "drop": ["PIECE_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "DRAGON_FANG", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "MONU_RID", "METEORIC_HORSESHOE"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["DRAGON_FANG", "DRAGON_FANG", "EXP_3_RID"] }
             ]
         },
         {
-            "qname": "[201808] Rider Training Ground - Intermediate", "ap": "20", "last-upd": "", "column": "", "icon": "rider-ushiwakamaru.png",
+            "qname": "[201808] Rider Training Ground - Intermediate", "ap": "20", "last-upd": "2018 08 16", "column": "18", "icon": "rider-ushiwakamaru.png",
             "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_RID", "PIECE_RID", "DRAGON_FANG", "DRAGON_FANG"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "PIECE_RID", "PIECE_RID", "EXP_2_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "GEM_BL_RID", "GEM_RD_RID", "PIECE_RID", "DRAGON_FANG", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "GEM_RD_RID", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "PIECE_RID", "MONU_RID", "DRAGON_FANG", "DRAGON_FANG"] }
             ]
         },
         {
-            "qname": "[201808] Rider Training Ground - Advanced", "ap": "30", "last-upd": "", "column": "", "icon": "rider-martha.png",
+            "qname": "[201808] Rider Training Ground - Advanced", "ap": "30", "last-upd": "2018 08 16", "column": "18", "icon": "rider-martha.png",
             "droplog": [
+                { "uplog": true, "drop": ["GEM_RD_RID", "GEM_RD_RID", "PIECE_RID", "DRAGON_FANG"] },
+                { "uplog": true, "drop": ["GEM_GD_RID", "GEM_GD_RID", "MONU_RID", "METEORIC_HORSESHOE", "EXP_1_RID"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "MONU_RID", "VOIDS_DUST"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "MONU_RID", "DRAGONS_REVERSE_SCALE", "EXP_3_RID"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "PIECE_RID", "PIECE_RID", "DRAGON_FANG", "DRAGON_FANG"] }
             ]
         },
         {
-            "qname": "[201808] Rider Training Ground - Expert", "ap": "40", "last-upd": "", "column": "", "icon": "rider-marie.png",
+            "qname": "[201808] Rider Training Ground - Expert", "ap": "40", "last-upd": "2018 08 16", "column": "18", "icon": "rider-marie.png",
             "droplog": [
+                { "uplog": true, "drop": ["GEM_GD_RID", "PIECE_RID", "MONU_RID", "DRAGONS_REVERSE_SCALE"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "GEM_RD_RID", "MONU_RID"] },
+                { "uplog": true, "drop": ["GEM_BL_RID", "PIECE_RID", "PIECE_RID", "MONU_RID", "DRAGON_FANG"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "GEM_RD_RID", "PIECE_RID"] },
+                { "uplog": true, "drop": ["GEM_RD_RID", "GEM_RD_RID", "GEM_GD_RID", "PIECE_RID", "EXP_3_RID"] }
             ]
         }
     ]
