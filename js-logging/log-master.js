cd9de78e3f4846bafa425681a6bbda83a2062f80
Andrew Fan <andrewsuicune@gmail.com>
2018-11-01 01:22:26 -0400

    Added W&P CE to drop list

--- js-logging/log-master.js
+++ js-logging/log-master.js
@@ -207,6 +207,7 @@ function initPathMap() {
     imgpathmap.push({"id": "HERO_BOW", "path": "./icon/ event/halloween2018/Hero Bow.png", "label": "HB", "qengine": "EVENT"});
     imgpathmap.push({"id": "HERO_MANTLE", "path": "./icon/ event/halloween2018/Hero Shield.png", "label": "HS", "qengine": "EVENT"});
     imgpathmap.push({"id": "HERO_SHIELD", "path": "./icon/ event/halloween2018/Hero Mantle.png", "label": "HM", "qengine": "EVENT"});
+    imgpathmap.push({"id": "CE_WIZARD_AND_PRIEST", "path": "./icon/ ce/Wizard and Priest.png", "label": "CE", "qengine": "EVENT-CE"});
 
 }
 
