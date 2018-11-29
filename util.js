8d05260456ee7f953d84962c3a5f0ef15446c306
Andrew Fan <andrewsuicune@gmail.com>
2018-11-29 10:50:16 -0500

    MORE EFFICIENCY NEEDED~

--- util.js
+++ util.js
@@ -45,6 +45,14 @@ var ID_SHORTCAKE = 1;
 var ID_FRUITCAKE = 2;
 var ID_MIRACLE_STOCKING = 3;
 
+function SCchristmas2018_PS() { // Add the drop ID context for the bare numbers; pipe them into the main function
+    var input = document.getElementById("christmas2018-sc-input-ps").value;
+    var drops = input.split(" ");
+    var output = "C2x" + drops[0] + ",C3x" + drops[1]+ ",S2x" + drops[2]+ ",S3x" + drops[3]+ ",M3x" + drops[4]+ ",M4x" + drops[5]
+    document.getElementById("christmas2018-sc-input").value = output;
+    SCchristmas2018();
+}
+
 function SCchristmas2018() {
     var input = document.getElementById("christmas2018-sc-input").value;
     var output = '<code>{ "uplog": false, "drop": [], "stackdrop": [';
