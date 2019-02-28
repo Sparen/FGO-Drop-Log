35e31a7cfaa2b10c858a0b50bddbf7cb604ca80b
Andrew Fan <andrewsuicune@gmail.com>
2019-02-28 10:59:37 -0500

    Skeletons for Shinjuku Free Quests added

--- js-logging/log-free-eor.js
+++ js-logging/log-free-eor.js
@@ -3,6 +3,55 @@
 var free_shinjuku_obj = {
     "cssclass": "h-shinjuku",
     "quests": [
-        
+        {
+            "qname": "Yoyogi 2-Chome: Mishmash Buildings", "ap": "20", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Route 20: Territory", "ap": "20", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Shinjuku Station: Concrete Dungeon", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Shinjuku 4-Chome: Hotel Street", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Kabukicho: Nightless Castle", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Barrel Tower: Rifling Hole", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Party Hall: Killing Party", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Tower - Top Floor: Breechloader", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Shinjuku Gyoen: Demon's Garden", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        },
+        {
+            "qname": "Shinjuku 2-Chome: Rainbow Town", "ap": "21", "last-upd": "", "column": "",
+            "droplog": [
+            ]
+        }
     ]
 };
