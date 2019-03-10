70631041f59cad1ae58ecbeba932ffb1df32c9bb
Andrew Fan <andrewsuicune@gmail.com>
2019-03-10 16:21:19 -0400

    Shinjuku Free Quests

--- js-logging/log-free-eor.js
+++ js-logging/log-free-eor.js
@@ -4,52 +4,64 @@ var free_shinjuku_obj = {
     "cssclass": "h-shinjuku",
     "quests": [
         {
-            "qname": "Yoyogi 2-Chome: Mishmash Buildings", "ap": "20", "last-upd": "", "column": "",
+            "qname": "Yoyogi 2-Chome: Mishmash Buildings", "ap": "20", "last-upd": "2019 03 10", "column": "20",
+            "droplog": [
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["GEM_BL_CAS"] },
+                { "uplog": true, "drop": ["GEM_BL_CAS", "GEM_BL_CAS"] }
+            ]
+        },
+        {
+            "qname": "Route 20: Territory", "ap": "20", "last-upd": "2019 03 10", "column": "20", "icon": "avenger-hessianlobo.png",
+            "droplog": [
+                { "uplog": true, "drop": ["MYSTIC_SPINAL_FLUID", "MYSTIC_SPINAL_FLUID"] },
+                { "uplog": true, "drop": ["MYSTIC_SPINAL_FLUID", "VOIDS_DUST"] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Shinjuku Station: Concrete Dungeon", "ap": "21", "last-upd": "2019 03 10", "column": "20",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_ARC", "GEM_BL_ARC", "GEM_RD_ARC", "GEM_RD_ARC", "MYSTIC_SPINAL_FLUID"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": [] }
+            ]
+        },
+        {
+            "qname": "Shinjuku 4-Chome: Hotel Street", "ap": "21", "last-upd": "2019 03 10", "column": "20",
+            "droplog": [
+                { "uplog": true, "drop": ["GEM_BL_CAS", "MYSTIC_SPINAL_FLUID"] },
+                { "uplog": true, "drop": [] },
+                { "uplog": true, "drop": ["CLAW_OF_CHAOS"] }
+            ]
+        },
+        {
+            "qname": "Kabukicho: Nightless Castle", "ap": "21", "last-upd": "", "column": "20",
             "droplog": [
             ]
         },
         {
-            "qname": "Route 20: Territory", "ap": "20", "last-upd": "", "column": "",
+            "qname": "Barrel Tower: Rifling Hole", "ap": "21", "last-upd": "", "column": "20",
             "droplog": [
             ]
         },
         {
-            "qname": "Shinjuku Station: Concrete Dungeon", "ap": "21", "last-upd": "", "column": "",
+            "qname": "Party Hall: Killing Party", "ap": "21", "last-upd": "", "column": "20",
             "droplog": [
             ]
         },
         {
-            "qname": "Shinjuku 4-Chome: Hotel Street", "ap": "21", "last-upd": "", "column": "",
+            "qname": "Tower - Top Floor: Breechloader", "ap": "21", "last-upd": "", "column": "20",
             "droplog": [
             ]
         },
         {
-            "qname": "Kabukicho: Nightless Castle", "ap": "21", "last-upd": "", "column": "",
+            "qname": "Shinjuku Gyoen: Demon's Garden", "ap": "21", "last-upd": "", "column": "20",
             "droplog": [
             ]
         },
         {
-            "qname": "Barrel Tower: Rifling Hole", "ap": "21", "last-upd": "", "column": "",
-            "droplog": [
-            ]
-        },
-        {
-            "qname": "Party Hall: Killing Party", "ap": "21", "last-upd": "", "column": "",
-            "droplog": [
-            ]
-        },
-        {
-            "qname": "Tower - Top Floor: Breechloader", "ap": "21", "last-upd": "", "column": "",
-            "droplog": [
-            ]
-        },
-        {
-            "qname": "Shinjuku Gyoen: Demon's Garden", "ap": "21", "last-upd": "", "column": "",
-            "droplog": [
-            ]
-        },
-        {
-            "qname": "Shinjuku 2-Chome: Rainbow Town", "ap": "21", "last-upd": "", "column": "",
+            "qname": "Shinjuku 2-Chome: Rainbow Town", "ap": "21", "last-upd": "", "column": "20",
             "droplog": [
             ]
         }
